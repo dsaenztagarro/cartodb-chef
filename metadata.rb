9f0a0dcc25f2a9c52587e06d0dbeef3a05d47d96
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-11-18 00:45:43 +0100

    Added MIT license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,9 @@
 name             'cartodb'
 maintainer       'David Saenz Tagarro'
 maintainer_email 'david.saenz.tagarro@gmail.com'
-license          'All rights reserved'
+license          'MIT'
 description      'Installs/Configures cartodb'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+supports 'ubuntu'
