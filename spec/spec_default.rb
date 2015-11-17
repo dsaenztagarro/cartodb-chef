537a2b016d72061e9a35693b9cf8047756b697f6
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-11-18 00:21:02 +0100

    Added default spec

---
+++ spec/spec_default.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'railsapp::default' do
+  let(:chef_run) do
+    ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '12.04') do |node|
+    end.converge described_recipe
+  end
+
+  it 'converges successfully' do
+    expect { :chef_run }.to_not raise_error
+  end
+end
