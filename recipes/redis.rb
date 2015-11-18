50ea4436d62e324fb8f3a321ee1326e584b6e480
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-11-18 12:35:01 +0100

    Added recipe to install redis

---
+++ recipes/redis.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: cartodb
+# Recipe:: redis
+#
+# Copyright 2015, David Saenz Tagarro
+#
+# All rights reserved - Do Not Redistribute
+#
+
+bash 'add-repo-redis' do
+  code <<-EOH
+    add-apt-repository ppa:cartodb/redis
+    apt-get update
+  EOH
+end
+
+execute 'install-redis' do
+  command 'apt-get install redis-server'
+end
